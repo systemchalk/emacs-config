b4bbaab922fb8248b5b588e43d9fbda0bff3ab56
systemchalk <systemchalk@gmail.com>
2026-08-28 17:32:05 -0400

    Redirect customzied output to custom.el

--- init.el
+++ init.el
@@ -3,4 +3,9 @@
 (defvar nm/init-loaded nil
   "Non-nil once init.el has run to completion.")
 
+;;;; Custom file -- Quarantines changes set within Emacs to untracked file
+
+(setopt custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror 'nomessage)
+
 (setq nm/init-loaded t)
